e409c0a2616ff539da14fae900726e9e581a981b
illumination-k <illumination.k.27@gmail.com>
2020-12-21 19:58:49 +0900

    add extract option and construct with filtering

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,6 @@ extern crate serde_derive;
 use std::{env::set_var};
 use std::path::PathBuf;
 
-use ndarray::Array2;
-use ndarray_stats::*;
-
 use structopt::{clap, StructOpt, clap::arg_enum};
 use anyhow::Result;
 
@@ -28,24 +25,17 @@ mod graph;
 mod codon;
 mod similarity;
 mod rank;
+mod handlers;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "hrr_corrnet")]
 #[structopt(long_version(option_env!("LONG_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"))))]
 #[structopt(setting(clap::AppSettings::ColoredHelp))]
 pub struct Opt {
-    #[structopt(short = "i", long = "input")]
-    pub input: PathBuf,
-    #[structopt(short = "t", long = "threads", default_value("1"), value_name = "INT")]
-    pub threads: u8,
     #[structopt(long = "log", possible_values(&LogLevel::variants()))]
     pub log_level: Option<LogLevel>,
-    #[structopt(long = "hrr_cutoff", default_value("30"), value_name = "INT")]
-    pub hrr_cutoff: usize,
-    #[structopt(long = "pcc_cutoff", value_name = "FLOAT")]
-    pub pcc_cutoff: Option<f64>,
-    #[structopt(short = "-o", long = "out_dir", default_value("hrr_results"))]
-    pub outdir: PathBuf,
+    #[structopt(subcommand)]
+    pub subcommand: SubCommands,
 }
 
 arg_enum! {
@@ -56,6 +46,46 @@ arg_enum! {
         WARN,
         ERROR,
     }
+}
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum Rank {
+        HRR,
+        MR,
+    }
+}
+
+#[derive(Debug, StructOpt)]
+pub enum SubCommands {
+    #[structopt(name = "construct", about="construct rank based network from gene expression matrix")]
+    #[structopt(setting(clap::AppSettings::ColoredHelp))]
+    Construct {
+        #[structopt(short = "-i", long = "input")]
+        input: PathBuf,
+        #[structopt(short = "-m", long = "method", possible_values(&Rank::variants()))]
+        method: Option<Rank>,
+        #[structopt(short = "-o", long = "output")]
+        output: Option<PathBuf>,
+        #[structopt(long = "rank_cutoff")]
+        rank_cutoff: Option<usize>,
+        #[structopt(long = "pcc_cutoff")]
+        pcc_cutoff: Option<f64>
+    },
+    #[structopt(name = "extract", about="extract")]
+    #[structopt(setting(clap::AppSettings::ColoredHelp))]
+    Extract {
+        #[structopt(short = "-i", long = "input")]
+        input: PathBuf,
+        #[structopt(short = "-g", long = "gene_list")]
+        gene_list: Option<PathBuf>,
+        #[structopt(short = "-o", long = "output")]
+        output: Option<PathBuf>,
+        #[structopt(long = "rank_cutoff")]
+        rank_cutoff: Option<f64>,
+        #[structopt(long = "pcc_cutoff")]
+        pcc_cutoff: Option<f64>
+    },
 }
 
 fn main() -> Result<()> {
@@ -74,34 +104,37 @@ fn main() -> Result<()> {
     };
     pretty_env_logger::init_timed();
 
-    info!("--- start read {}  ---", &opt.input.as_path().to_str().unwrap());
-
-    // read csv and make ndarray::Array2
-    let mut index: Vec<String> = vec![];
-    
-    let arr = io::read_exp_csv(&opt.input, &mut index)?;
-
-    // calc correlation
-    let corr = arr.pearson_correlation()?;
-    debug!("{:?}", corr.shape());
-    debug!("corr_matrix: \n{:?}", corr);
-    debug!("corr[0, 4]: {} {} : {:?}", &index[0], &index[4], corr[[0, 4]]);
-
-    // calc rank matrix
-    info!("calculate rank matrix...");
-    let array_size = index.len();
-    let rank_arr: Array2<usize> = rank::construct_rank_matrix(&corr, array_size)?;
-    // construct hrr based network
-    info!("construct hrr based network...");
-    let hrr_cutoff: usize = *(&opt.hrr_cutoff);
-    let pcc_cutoff: Option<f64> = *(&opt.pcc_cutoff);
-    let mut graph: graph::Graph<usize> = graph::Graph::new(&index);
-    graph.construct_hrr_network(corr, rank_arr, hrr_cutoff, pcc_cutoff);
-
-    // calc hcca clusters
-
-    info!("Write to csv...");
-    io::graph_to_csv("ignore/graph.csv", graph)?;
-    // calc codon usage
+    match &opt.subcommand {
+        SubCommands::Construct {
+            input,
+            output,
+            method,
+            rank_cutoff,
+            pcc_cutoff
+        } => {
+            handlers::construct::parse_args(
+                input, 
+                output.as_ref(), 
+                method.as_ref(),
+                rank_cutoff.as_ref(),
+                pcc_cutoff.as_ref(),
+            )?;
+        },
+        SubCommands::Extract {
+            input,
+            gene_list,
+            output,
+            rank_cutoff,
+            pcc_cutoff
+        } => {
+            handlers::extract::parse_args(
+                input, 
+                gene_list.as_ref(), 
+                output.as_ref(), 
+                rank_cutoff.as_ref(), 
+                pcc_cutoff.as_ref()
+            )?;
+        }
+    }
     Ok(())
 }
