4e8906792c7a249ef0d4d73dae33a369084b3fae
illumination-k <illumination.k.27@gmail.com>
2020-12-23 19:26:48 +0900

    supress warning

--- src/graph.rs
+++ src/graph.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::{Display, Debug}, str::FromStr, vec};
+use std::fmt::{Display, Debug};
 use ndarray::{Array2};
 
 use crate::io;
@@ -65,7 +65,8 @@ impl<T> Edge<T>
     pub fn query(&self) -> usize {
         self.node_1
     }
-
+    
+    #[allow(dead_code)]
     pub fn target(&self) -> usize {
         self.node_2
     }
